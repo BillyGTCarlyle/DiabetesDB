c7cd3f34dce851c42b5f58ec6532530cdc500767
Billy Carlyle <billy@billycarlyle.uk>
2023-06-27 21:05:47 +0100

    Added meal class for database logging.  Added glucose level to the addMeal UI as well as some labels.

---
+++ app/src/main/java/com/billycarlyle/diabetesdb/Meal.java
@@ -0,0 +1,66 @@
+package com.billycarlyle.diabetesdb;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity (tableName = "meal_table")
+public class Meal {
+    @PrimaryKey
+    @NonNull
+    String carbType;
+    float carbCount;
+    float glucoseLevelStart;
+    float glucoseLevel1h;
+    boolean withFat;
+    boolean exerciseDay;
+    public Meal(String carbType, float carbCount, float glucoseLevelStart, boolean withFat, boolean exerciseDay){
+        this.carbType = carbType;
+        this.carbCount = carbCount;
+        this.glucoseLevelStart = glucoseLevelStart;
+        this.withFat = withFat;
+        this.exerciseDay = exerciseDay;
+    }
+
+    public String getcarbType(){
+        return carbType;
+    }
+    public void setCarbType(String carbType){
+        this.carbType = carbType;
+    }
+
+    public float getCarbCount(){
+        return carbCount;
+    }
+    public void setCarbCount(float carbCount){
+        this.carbCount = carbCount;
+    }
+
+    public float getGlucoseLevelStart(){
+        return glucoseLevelStart;
+    }
+    public void setGlucoseLevelStart(float glucoseLevelStart){
+        this.glucoseLevelStart = glucoseLevelStart;
+    }
+
+    public float getGlucoseLevel1h(){
+        return glucoseLevel1h;
+    }
+    public void setGlucoseLevel1h(float glucoseLevel1h){
+        this.glucoseLevel1h = glucoseLevel1h;
+    }
+
+    public boolean isWithFat(){
+        return withFat;
+    }
+    public void setWithFat(boolean withFat){
+        this.withFat = withFat;
+    }
+
+    public boolean isExerciseDay(){
+        return exerciseDay;
+    }
+    public void setExerciseDay(boolean exerciseDay){
+        this.exerciseDay = exerciseDay;
+    }
+}
